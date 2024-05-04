a5d92e0c5c71adac1a95c4197052bd8ec100cdea
AndIsaev <and.isaev.v@yandex.ru>
2024-05-04 22:38:28 +0300

    added test for client

--- internal/service/agent/client/send_metrics.go
+++ internal/service/agent/client/send_metrics.go
@@ -2,18 +2,15 @@ package client
 
 import (
 	"bytes"
-	"errors"
 	"log"
 	"net/http"
 )
 
 func SendMetricsClient(url, contentType string, body []byte) error {
-	ErrorSendMetricsHandler := errors.New("connection error")
-
 	resp, err := http.Post(url, contentType, bytes.NewBuffer(body))
 	if err != nil {
 		log.Fatalf("an Error Occurred %v", err)
-		return ErrorSendMetricsHandler
+		return nil
 	}
 
 	defer resp.Body.Close()
