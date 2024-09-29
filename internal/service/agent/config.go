05ac72c40c5f55cf54c2abe0e3c55f088c5fbabe
AndIsaev <and.isaev.v@yandex.ru>
2024-09-29 18:08:31 +0300

    added RATE_LIMIT

--- internal/service/agent/config.go
+++ internal/service/agent/config.go
@@ -15,6 +15,7 @@ type Config struct {
 	ReportInterval       time.Duration `env:"REPORT_INTERVAL"`
 	PollInterval         time.Duration `env:"POLL_INTERVAL"`
 	Key                  string        `env:"KEY"`
+	RateLimit            uint64        `env:"RATE_LIMIT"`
 	StorageMetrics       *metrics.StorageMetrics
 	UpdateMetricAddress  string
 	UpdateMetricsAddress string
@@ -25,13 +26,23 @@ func NewConfig() *Config {
 	cfg := &Config{StorageMetrics: metrics.NewListMetrics(), ProtocolHTTP: "http"}
 	var pollIntervalSeconds uint64
 	var reportIntervalSeconds uint64
+	var rateLimit uint64
 
 	flag.StringVar(&cfg.Address, "a", "localhost:8080", "address")
 	flag.Uint64Var(&reportIntervalSeconds, "r", 10, "seconds of report interval")
 	flag.Uint64Var(&pollIntervalSeconds, "p", 2, "seconds of poll interval")
 	flag.StringVar(&cfg.Key, "k", "", "set key")
+	flag.Uint64Var(&rateLimit, "l", 1, "rate limit")
 
 	flag.Parse()
+
+	if envRateLimit := os.Getenv("RATE_LIMIT"); envRateLimit != "" {
+		if val, err := strconv.Atoi(envRateLimit); err == nil {
+			cfg.RateLimit = uint64(val)
+		}
+	} else {
+		cfg.RateLimit = rateLimit
+	}
 
 	if envKey := os.Getenv("KEY"); envKey != "" {
 		cfg.Key = envKey
