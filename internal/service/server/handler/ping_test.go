a1d50beafeb884dc8e0e12a161f9583aec58d743
AndIsaev <and.isaev.v@yandex.ru>
2025-02-05 23:09:02 +0300

    added tests for ping handler

--- internal/service/server/handler/ping_test.go
+++ internal/service/server/handler/ping_test.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func ExampleHandler_PingHandler() {
@@ -23,4 +27,41 @@ func ExampleHandler_PingHandler() {
 
 	// Output:
 	// Status Code: 200
+}
+
+func TestPingHandler(t *testing.T) {
+	testMock := setupTest(t)
+	handler := &Handler{MetricService: testMock.mockService}
+
+	tests := []struct {
+		name               string
+		expectedStatus     int
+		expectedResultFunc error
+	}{
+		{
+			name:               "success ping",
+			expectedStatus:     http.StatusOK,
+			expectedResultFunc: nil,
+		},
+		{
+			name:               "error ping",
+			expectedStatus:     http.StatusInternalServerError,
+			expectedResultFunc: errors.New("ping error"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+
+			testMock.mockService.EXPECT().
+				PingStorage(req.Context()).
+				Return(tt.expectedResultFunc)
+
+			handler.PingHandler().ServeHTTP(rec, req)
+
+			assert.Equal(t, tt.expectedStatus, rec.Code)
+		})
+	}
 }
